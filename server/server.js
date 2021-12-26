1d4c5f975f0c4c8e2e33bf5229b42c9bd6a662f1
zeyneparslan55 <58356584+zeyneparslan55@users.noreply.github.com>
2021-12-26 04:05:44 +0300

    server.js added

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const http = require('http').createServer();
+const io = require('socket.io')(http);
+
+http.listen(3000);
+io.on('connection',socket=>{
+    console.log('bir kullanıcı bağlandı');
+
+    socket.on('disconnected', () =>{
+        console.log('bir kullanıcı ayrıldı')
+    });
+});
