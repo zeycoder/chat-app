0f82fd2010063adbacb362f1dbffee3a23665759
zeyneparslan55 <58356584+zeyneparslan55@users.noreply.github.com>
2021-12-27 02:57:47 +0300

    edited

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,23 @@
 <template>
   <div id="app">
+    <div v-if="!login">
+      <form @submit.prevent="enterName()">
+        <input v-model="name" placeholder="Adınız" >
+        <input type="submit" value="Giriş Yap" >
+      </form>
+    </div>
+    <div v-else>
+      Online kullanıcılar
+      <ul>
+        <li>test</li>
+        <li>test</li>
+      </ul>
+      Mesajlar
+      <ul>
+        <li>mesaj</li>
+        <li>mesaj</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -7,6 +25,14 @@
 export default {
   data () {
     return {
+      login:false,
+      name:""
+    }
+  },
+  methods:{
+    enterName(){
+      this.login=true
+      this.$socket.emit('new_user',this.name);
     }
   }
 }
--- server/server.js
+++ server/server.js
@@ -2,8 +2,19 @@ const http = require('http').createServer();
 const io = require('socket.io')(http);
 
 http.listen(3000);
+
+let users = [];
+
 io.on('connection',socket=>{
     console.log('bir kullanıcı bağlandı');
+
+    socket.on('new_user',(name)=>{
+        users.push({
+            id: socket.id,
+            name
+        });
+        io.emit('users',users);
+    });
 
     socket.on('disconnect', () =>{
         console.log('bir kullanıcı ayrıldı')
